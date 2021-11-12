ed42ad2d88a571513b66ec794986717fd0e1ac27
eidisato <viniciuseidi@outlook.com>
2021-11-11 22:17:06 -0300

    table user and, security and service

--- blogPessoal/src/main/java/br/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/br/org/generation/blogPessoal/model/Postagem.java
@@ -33,6 +33,10 @@ public class Postagem {
 @JsonIgnoreProperties("postagem")
 private Tema tema;
 
+@ManyToOne
+@JsonIgnoreProperties("postagem")
+private Usuario usuario;
+
  
 public long getId() {
 	return id;
